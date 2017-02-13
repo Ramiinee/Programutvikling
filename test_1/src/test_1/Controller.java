ca993ed76dd85b9d84748b12a2e51d6d0de62177
Meyben <ida@honningbier.no>
2017-02-13 10:55:42 +0100

    Merge origin/Her_Jobber_Vi into Ida
    Conflicts:
    	test_1/src/test_1/TEst

--- test_1/src/test_1/Controller.java
+++ test_1/src/test_1/Controller.java
@@ -15,22 +15,28 @@ public class Controller {
     public int b;
     @FXML
     private Canvas graphics;
+    
+    
+    // sette en random størrelse på disse? 
+    public int kolonner = 10;
+    public int rader = 10;
 
 
-    private int[][] board = {
-            { 1, 1, 1, 1 },
-            { 1, 1, 1, 1 },
-            { 1, 1, 1, 0 },
-            { 1, 0, 0, 1 }
-    };
+
+    private int[][] board = new int[kolonner][rader];
+   
+    
     private GraphicsContext gc;
 
     @FXML private void draw_Array(){
         
         
         gc = graphics.getGraphicsContext2D();
-        for (i = 0; i < board.length; i++) {
-            for (j = 0; j < board.length ; j++) {
+        for (i = 0; i < kolonner; i++) {
+            for (j = 0; j < rader ; j++) {
+                
+                board[i][j] = (Math.random()<0.5)?0:1; 
+                
                 if (board[i][j] == 1){
 
                     draw( i , j, Color.BLACK );
@@ -39,18 +45,31 @@ public class Controller {
             
         
         int neighbors = 0;
-            
+            if (i != 0 && j !=0){
             if (board[i-1][j-1] == 1) neighbors++;
+            }
+            if (j != 0){
             if (board[i][j-1] == 1) neighbors++;
+            }
+            if (i != board[i].length && j != 0 ){
             if (board[i+1][j-1] == 1) neighbors++;
- 
+            }
+            
+            if (i != 0){
             if (board[i-1][j]   == 1) neighbors++;
+            }
+            if (i != board[i].length){
             if (board[i+1][j]   == 1) neighbors++;
- 
+            }
+            if(i != 0 && j != board[j].length){
             if (board[i-1][j+1] == 1) neighbors++;
+            }
+            if(j != board[j].length){
             if (board[i][j+1] == 1) neighbors++;
+            }
+            if(i != board[i].length && j != board[j].length){
             if (board[i+1][j+1] == 1) neighbors++;
-
+            }
             
             for (a = -1; a <= 1; a++) {
                  for (b = -1; b <= 1; b++) {
