320cf7480525390e0e8c870dae2c43ff92963a32
Joacber <s302095@stud.hioa.no>
2017-02-16 13:35:12 +0100

    t

---
+++ test_1/src/Game/Controller.java
@@ -0,0 +1,184 @@
+package Game;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
+public class Controller implements Initializable{
+
+    
+    
+    public int timing = 120;
+    Timeline timeline = new Timeline( new KeyFrame(Duration.millis(timing), e -> draw_Array()));
+
+    @FXML
+    private Canvas Canvas;
+    private GraphicsContext gc;
+
+
+    // sette en random størrelse på disse?
+    public int kolonner = 50;
+    public int rader = 40;
+    
+    private int[][] board = new int[kolonner][rader];
+    public int[][] nextgeneration;
+    
+    
+    
+    
+    
+    
+    public void timeline(){
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.setAutoReverse(false);
+    }
+
+   
+
+    public void startButton(){
+        randomPattern();
+        draw_Array();
+        timeline();
+        timeline.play();
+
+    }
+    
+    public void randomPattern(){
+        for (int i = 0; i < kolonner; i++) {
+            for (int j = 0; j < rader ; j++) {
+
+                board[i][j] = (Math.random()<0.5)?0:1;
+            }}}
+    private void draw_Array(){
+        for (int i = 0; i < kolonner; i++) {
+            for (int j = 0; j < rader ; j++) {
+
+                if (board[i][j] == 1){
+
+                    draw( i , j, Color.BLACK);
+
+                }
+                if (board[i][j] == 0){
+                    draw(i, j, Color.WHITE);
+                }
+            }
+        }
+
+        // ----------------------------
+
+
+        nextgeneration = new int[kolonner][rader];
+        for (int a = 0; a < board.length; a++) {
+            for (int b = 0; b < board[a].length; b++) {
+                int neighbors = 0;
+                if (a != 0 && b !=0){
+                    if (board[a-1][b-1] == 1)
+                        neighbors++;
+                }
+                if (b != 0){
+                    if (board[a][b-1] == 1)
+                        neighbors++;
+                }
+                try {
+                    if (a != board[a].length -1 && b != 0 ){
+                        if (board[a+1][b-1] == 1)
+                            neighbors++;
+                    }
+                }catch (ArrayIndexOutOfBoundsException e){}
+
+                if (a != 0){
+                    if (board[a-1][b]   == 1)
+                        neighbors++;
+                }
+                try {
+                    if (a != board[a].length -1){
+                        if (board[a+1][b]   == 1)
+                            neighbors++;
+                    }
+                }catch (ArrayIndexOutOfBoundsException e) {}
+
+                if(a != 0 && b != board[b].length -1){
+                    if (board[a-1][b+1] == 1)
+                        neighbors++;
+                }
+                if(b != board[b].length -1){
+                    if (board[a][b+1] == 1)
+                        neighbors++;
+                }
+                try {
+                    if(a != board[a].length - 1 && b != board[b].length -1){
+                        if (board[a+1][b+1] == 1)
+                            neighbors++;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                }
+
+
+                //----------------------------
+
+                if(neighbors <  2) {
+                    nextgeneration[a][b] = 0;
+                }
+                else if (neighbors >  3) {
+                    nextgeneration[a][b] = 0;
+                }
+                else if (neighbors == 3) {
+                    nextgeneration[a][b] = 1;
+                }
+                else {
+                    nextgeneration[a][b] = board[a][b];
+                }
+                //System.out.print(board[a][b] + "  ");
+
+            }
+            //System.out.println();
+
+        }
+        //System.out.println();
+
+        board = nextgeneration;
+
+    }
+
+
+
+    @FXML private void remove_Array() {
+        gc = Canvas.getGraphicsContext2D();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length ; j++) {
+
+                if (board[i][j] == 1){
+
+                    draw( i , j, Color.WHITE );
+
+                }
+
+
+            }
+
+        }
+    }
+
+    private void draw( int x, int y, Color c) {
+        gc.setFill(c);
+        gc.fillRect(x*9 , y*9, 8, 8);
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gc = Canvas.getGraphicsContext2D();
+        System.out.println("test");
+
+    }
+
+
+}
